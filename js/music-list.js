5e4f0f5c0cf09aa6b0e15a0fb192e7e1140bce8e
Hfzsulfikar <95353376+HfzzSulfikar@users.noreply.github.com>
2023-07-15 15:07:41 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,35 @@
+
+let allMusic = [
+  
+  {
+    name: "Surah Yaseen",
+    artist: "Hafiz Sulfikar",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Ayathul Kursi",
+    artist: "Hafiz Sulfikar",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "From Surah Kahf",
+    artist: "Hafiz Sulfikar",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "From Surah Baqara",
+    artist: "Hafiz Sulfikar",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "From Surah Ahzab",
+    artist: "Hafiz Sulfikar",
+    img: "music-6",
+    src: "music-6"
+  },
+  
+];
